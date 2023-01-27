323a9e9cf5345828bcc006926f3eab25afe78ea5
Alastair Crabtree <a.crabtree@imperial.ac.uk>
2023-01-27 14:53:12 +0000

    task: lint and tidy

--- tests/test_check_gaps.py
+++ tests/test_check_gaps.py
@@ -15,28 +15,32 @@ report_file = Path('gap-report.txt')
 default_command_params = ["check-gap", "--mode", "BurstE128", "sample-data/example.csv"]
 alt_command_params = ["check-gap", "-f", "-m", "normalE8", "sample-data/example.csv"]
 
-@pytest.fixture(autouse=True)
+
+@pytest.fixture(autouse=True)  # or use something like @pytest.mark.usefixtures("run_around_tests")
 def run_around_tests():
-    if(report_file.exists()):
+
+    if (report_file.exists()):
         os.remove(report_file.absolute())
 
     yield
 
-    if(report_file.exists()):
+    if (report_file.exists()):
         os.remove(report_file.absolute())
 
-#@pytest.mark.usefixtures("run_around_tests")
+
 def test_check_gap_creates_report():
 
     result = runner.invoke(app, default_command_params)
     assert result.exit_code == 0
     assert report_file.exists
 
+
 def test_check_gap_creates_report_with_alternative_arguments():
 
     result = runner.invoke(app, alt_command_params)
     assert result.exit_code == 0
     assert report_file.exists
+
 
 def test_check_gap_will_not_overwrite_report():
 
@@ -48,6 +52,7 @@ def test_check_gap_will_not_overwrite_report():
     assert result.exit_code == 1
     assert "gap-report.txt already exists - delete file or use --force" in result.stdout
 
+
 def test_check_gap_will_overwrite_report_when_forced():
 
     with open(report_file.absolute(), 'w'):
@@ -57,12 +62,14 @@ def test_check_gap_will_overwrite_report_when_forced():
 
     assert result.exit_code == 0
 
+
 def test_check_gap_finds_invalid_seqence_counter():
 
     result = runner.invoke(app, ["check-gap", "--mode", "normalE8", "sample-data/normal_data20230112-11h23-bad-sequence.csv"])
 
     assert "Non sequential packet detected! line number 34, sequence count: 99, vector number 1" in result.stdout
     assert result.exit_code == 2
+
 
 def test_check_gap_finds_invalid_seqence_counter_within_packet():
 
@@ -71,12 +78,14 @@ def test_check_gap_finds_invalid_seqence_counter_within_packet():
     assert "Non sequential packet detected! line number 4, sequence count: 99" in result.stdout
     assert result.exit_code == 2
 
+
 def test_check_gap_has_no_errors_for_valid_burst_data():
 
     result = runner.invoke(app, ["check-gap", "sample-data/burst_data20230112-11h23.csv"])
 
     assert result.exit_code == 0
     assert "Gap checker complete successfully. Checked 1 packet(s) across 513 lines." in result.stdout
+
 
 def test_check_gap_finds_invalid_if_in_wrong_mode():
 
